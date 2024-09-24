d34940a67e559d74c8d05eff37fdfa79fd5b3d60
Mark Mwima <markmwimar@gmail.com>
2024-09-24 23:53:32 +0300

    main component

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+  
+  <main>
+  <header></header>
+  </main>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
