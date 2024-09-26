0260139263755b835405b1de53ec01fb84aacd49
Mwimar <markmwimar@gmail.com>
2024-09-26 22:34:40 +0300

    housing location interface data

--- src/app/housing-location.ts
+++ src/app/housing-location.ts
@@ -1,2 +1,11 @@
 export interface HousingLocation {
+    id:number,
+    name:string,
+    city:string,
+    state:string,
+    image:string,
+    availableUnits:number,
+    wifi:boolean,
+    laundry:boolean,
 }
+
