53cb00a4c328f079f8f1d825aa7275c665192c3f
Mwimar <markmwimar@gmail.com>
2024-09-26 23:02:26 +0300

    housing location list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 
 @Component({
@@ -22,5 +23,6 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  housingLocationList:HousingLocation[] =[]
 
 }
