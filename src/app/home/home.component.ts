e4c4f5d8324fd463cef9dfe3c47c80c2b2d97445
Mwimar <markmwimar@gmail.com>
2024-09-26 23:24:29 +0300

    serving images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,12 +17,43 @@ import { HousingLocation } from '../housing-location';
    </section>
 
    <section class="results">
-   <app-housing-location></app-housing-location>
+   <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
    </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  housingLocationList:HousingLocation[] =[]
+  housingLocationList:HousingLocation[] =[
+    {
+      "id":0,
+      "name": "Commfort Homes",
+      "city": "Nairobi",
+      "state": "NRB",
+      "photo": "/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
+      "availableUnits":4,
+      "wifi": true,
+      "laundry": true,
+    },
+    {
+      "id":1,
+      "name": "Beach House",
+      "city": "Mombasa",
+      "state": "MSA",
+      "photo": "/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
+      "availableUnits":2,
+      "wifi": true,
+      "laundry": true,
+    },
+    {
+      "id":3,
+      "name": "Luxury Apartments",
+      "city": "Busia",
+      "state": "BSA",
+      "photo": "/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
+      "availableUnits":10,
+      "wifi": true,
+      "laundry": true,
+    }
+  ]
 
 }
