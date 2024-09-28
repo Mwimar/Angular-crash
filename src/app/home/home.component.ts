ac0b9d843cf46e71b24dac3c5472f30c297ab971
Mwimar <markmwimar@gmail.com>
2024-09-28 20:45:54 +0300

    injecting housing service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 
 @Component({
@@ -23,8 +24,8 @@ import { HousingLocation } from '../housing-location';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  housingLocationList:HousingLocation[] =[
-    
-  ]
+  housingLocationList:HousingLocation[] =[];
+  housingService:HousingService = inject(HousingService);
+  
 
 }
